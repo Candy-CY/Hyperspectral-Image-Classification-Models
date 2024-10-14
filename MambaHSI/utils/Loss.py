44059f050856cd932f93437655880e2ede65fb81
只写Bug的坎迪 <65268800+Candy-CY@users.noreply.github.com>
2024-10-14 10:37:53 +0800

    Create Loss.py

---
+++ MambaHSI/utils/Loss.py
@@ -0,0 +1,35 @@
+import warnings
+import torch.nn.functional as F
+
+
+def resize(input,
+           size=None,
+           scale_factor=None,
+           mode='nearest',
+           align_corners=None,
+           warning=True):
+    if warning:
+        if size is not None and align_corners:
+            input_h, input_w = tuple(int(x) for x in input.shape[2:])
+            output_h, output_w = tuple(int(x) for x in size)
+            if output_h > input_h or output_w > output_h:
+                if ((output_h > 1 and output_w > 1 and input_h > 1
+                     and input_w > 1) and (output_h - 1) % (input_h - 1)
+                        and (output_w - 1) % (input_w - 1)):
+                    warnings.warn(
+                        f'When align_corners={align_corners}, '
+                        'the output would more aligned if '
+                        f'input size {(input_h, input_w)} is `x+1` and '
+                        f'out size {(output_h, output_w)} is `nx+1`')
+    return F.interpolate(input, size, scale_factor, mode, align_corners)
+
+
+def head_loss(loss_func,logits,label,align_corners=True):
+    seg_logits = resize(
+        input=logits,
+        size=label.shape[1:],
+        mode='bilinear',
+        align_corners=align_corners)
+
+    loss = loss_func(seg_logits,label)
+    return loss
